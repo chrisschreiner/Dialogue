53fd87456954f1e2fc819d9ddfcc64a71a397399
Chris Patrick Schreiner <chris.patrick.schreiner@gmail.com>
2015-10-23 08:27:05 +0200

    Initial Commit

---
+++ Dialogue/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dialogue
+//
+//  Created by Chris Patrick Schreiner on 23/10/15.
+//  Copyright © 2015 Chris Patrick Schreiner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
